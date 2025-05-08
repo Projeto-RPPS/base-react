5d79a187f250788a4cc86dbbfc6c40825634ee24
João Marcos <joaomcruz10@gmail.com>
2025-05-07 22:37:08 -0300

    ajuste imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import CreateLoan from './forms/emprestimoApi/emprestimo/CreateLoan'
 import LoanList from './forms/emprestimoApi/emprestimo/LoanList'
 import SimulateLoan from './forms/emprestimoApi/emprestimo/SimulateLoan'
+import SimulateMargin from './forms/emprestimoApi/margemCosignavel/SimulateMargin'
 
 export default function App() {
-  return <SimulateLoan />
+  return <CreateLoan />
 }
